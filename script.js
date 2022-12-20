9b627b89bc6f922b991cf499880c8886dcec52e8
Gijs van de Putte <112858394+GijsvdP21@users.noreply.github.com>
2022-12-20 14:51:38 +0100

    update searchbar

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ document.querySelector("#share-button").addEventListener("click", function () {
     });
 });
 
+
+function search_animal() {
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('alle-tekst');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "block";
+        }
+    }
+}
